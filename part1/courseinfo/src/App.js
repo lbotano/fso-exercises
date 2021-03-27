12282245794bc3c122c6d80a2c9be7b355809450
Lautaro Bernabé Otaño <otanolautarob@gmail.com>
2021-03-27 00:05:01 -0300

    Completed exercise 1.3

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -17,9 +17,9 @@ const Part = (props) => {
 const Content = (props) => {
     return (
     <div>
-        <Part caption={props.part1} value={props.exercises1} />
-        <Part caption={props.part2} value={props.exercises2} />
-        <Part caption={props.part3} value={props.exercises3} />
+        <Part caption={props.part1.name} value={props.part1.exercises} />
+        <Part caption={props.part2.name} value={props.part2.exercises} />
+        <Part caption={props.part3.name} value={props.part3.exercises} />
     </div>
     )
 }
@@ -32,22 +32,28 @@ const Total = (props) => {
 
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
+    const part1 = {
+        name: 'Fundamentals of React',
+        exercises: 10
+    }
+    const part2 = {
+        name: 'Using props to pass data',
+        exercises: 7
+    }
+    const part3 = {
+        name: 'State of a component',
+        exercises: 14
+    }
 
     return (
     <div>
         <Header course={course} />
         <Content
-            part1={part1} exercises1={exercises1}
-            part2={part2} exercises2={exercises2}
-            part3={part3} exercises3={exercises3}
+            part1={part1}
+            part2={part2}
+            part3={part3}
         />
-        <Total number={exercises1 + exercises2 + exercises3} />
+        <Total number={part1.exercises + part2.exercises + part3.exercises} />
     </div>
     )
 }
