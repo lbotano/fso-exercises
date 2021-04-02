b02fb8c1818f7d9553e110395989d4b87a85134a
Lautaro Bernabé Otaño <otanolautarob@gmail.com>
2021-04-02 12:51:52 -0300

    Corrected exercise 2.1

--- part2/kurssitiedot/src/index.js
+++ part2/kurssitiedot/src/index.js
@@ -27,9 +27,10 @@ const Part = (props) => {
 const Content = ({ course }) => {
     return (
         <div>
-            <Part part={course.parts[0]} />
-            <Part part={course.parts[1]} />
-            <Part part={course.parts[2]} />
+            {
+                course.parts.map((part) =>
+                    <Part part={part} key={part.id} />)
+            }
         </div>
     )
 }
