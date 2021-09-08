42e727a64eec5b92f8b0abb9f01eacb328acd425
Lautaro Bernabé Otaño <otanolautarob@gmail.com>
2021-09-08 18:32:48 -0300

    Completed exercise 8.16

--- part8/library/index.js
+++ part8/library/index.js
@@ -1,30 +1,43 @@
 const { ApolloServer, gql, UserInputError } = require('apollo-server')
 const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
 
 const Author = require('./schemas/authors')
 const Book = require('./schemas/books')
+const User = require('./schemas/users')
 
 const typeDefs = gql`
   type Author {
-    id : ID!
-    name : String!
-    born : Int
-    bookCount : Int!
+    id: ID!
+    name: String!
+    born: Int
+    bookCount: Int!
   }
 
   type Book {
-    id : ID!
-    title : String!
-    author : Author!
-    published : Int!
+    id: ID!
+    title: String!
+    author: Author!
+    published: Int!
     genres: [String!]!
   }
 
+  type User {
+    username: String!
+    favoriteGenre: String!
+    id: ID!
+  }
+
+  type Token {
+    value: String!
+  }
+
   type Query {
-    bookCount : Int!
-    authorCount : Int!
-    allBooks(author : String, genre : String) : [Book!]!
-    allAuthors : [Author!]!
+    bookCount: Int!
+    authorCount: Int!
+    allBooks(author: String, genre: String): [Book!]!
+    allAuthors: [Author!]!
+    me: User
   }
 
   type Mutation {
@@ -33,11 +46,19 @@ const typeDefs = gql`
       author : String!
       published : Int!
       genres : [String!]!
-    ) : Book!,
+    ): Book!
     editAuthor(
       name : String!,
       setBornTo : Int!,
-    ) : Author
+    ): Author
+    createUser(
+      username: String!
+      favoriteGenre: String!
+    ): User
+    login(
+      username: String!
+      password: String!
+    ): Token
   }
 `
 
@@ -48,11 +69,16 @@ const resolvers = {
   Query: {
     bookCount: async () => await Book.estimatedDocumentCount(),
     authorCount: async () => await Author.estimatedDocumentCount(),
-    allBooks: async (root, args) => await Book.find(),
-    allAuthors: async () => await Author.find()
+    allBooks: async (root, args) => await Book.find().populate('author'),
+    allAuthors: async () => await Author.find(),
+    me: (root, args, context) => context.currentUser
   },
   Mutation: {
-    addBook: async (root, args) => {
+    addBook: async (root, args, context) => {
+      if (!context.currentUser) {
+        throw new UserInputError('not logged in')
+      }
+
       let author = await Author.findOne({ name: args.author })
       if (!author) {
         author = await new Author({ name: args.author })
@@ -71,7 +97,11 @@ const resolvers = {
       }
       return newBook
     },
-    editAuthor: async (root, args) => {
+    editAuthor: async (root, args, context) => {
+      if (!context.currentUser) {
+        throw new UserInputError('not logged in')
+      }
+
       try {
         return await Author.findOneAndUpdate(
           { name: args.name },
@@ -82,6 +112,30 @@ const resolvers = {
           invalidArgs: args
         })
       }
+    },
+    createUser: async (root, args) => {
+      const user = new User({ username: args.username, favoriteGenre: args.favoriteGenre })
+
+      return user.save()
+        .catch((error) => {
+          throw new UserInputError(error.message, {
+            invalidArgs: args
+          })
+        })
+    },
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username })
+
+      if (!user || args.password !== 'secret') {
+        throw new UserInputError('wrong credentials')
+      }
+
+      const userForToken = {
+        username: user.username,
+        id: user._id
+      }
+
+      return { value: jwt.sign(userForToken, process.env.JWT_SECRET) }
     }
   }
 }
@@ -89,6 +143,16 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: async ({ req }) => {
+    const auth = req ? req.headers.authorization : null
+    if (auth && auth.toLowerCase().startsWith('bearer ')) {
+      const decodedToken = jwt.verify(
+        auth.substring(7), process.env.JWT_SECRET
+      )
+      const currentUser = await User.findById(decodedToken.id)
+      return { currentUser }
+    }
+  }
 })
 
 mongoose.connect(process.env.MONGODB_URI).then(() => {
